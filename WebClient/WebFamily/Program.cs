5180865d2f80d806e1cb3cf46b643b451ae8de79
Загарий Дмитрий Сергеевич (zagariy_ds) <zagariy_ds@nlmk.com>
2026-05-18 13:59:07 +0300

    awdwad

--- WebClient/WebFamily/Program.cs
+++ WebClient/WebFamily/Program.cs
@@ -10,7 +10,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5274/family") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5274") });
 builder.Services.AddBlazorBootstrap();
 builder.Services.AddBlazorFamilyJS();
 builder.Services.AddScoped<FamilyMappingService>();
